6aba73544de6fb447c21605ff4adbcac8944b8fa
ostxen <ostxen@gmail.com>
2023-11-11 22:01:28 +0800

    HTTP_C#_异步上传文件

--- Assets/Scripts/Lesson25_HTTP_C#_下载数据/Lesson25ExerciseTest.cs
+++ Assets/Scripts/Lesson25_HTTP_C#_下载数据/Lesson25ExerciseTest.cs
@@ -8,13 +8,21 @@ public class Lesson25ExerciseTest: MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print(Application.persistentDataPath);
-        HttpMgr.Instance.DownLoadFileAsync("技能的本质.png", Application.persistentDataPath + "/技能的本质http.jpg", (code) =>
+        //print(Application.persistentDataPath);
+        //HttpMgr.Instance.DownLoadFileAsync("技能的本质.png", Application.persistentDataPath + "/技能的本质http.jpg", (code) =>
+        //{
+        //    if (code == HttpStatusCode.OK)
+        //        print("下载成功");
+        //    else
+        //        print("下载失败" + code);
+        //});
+
+         HttpMgr.Instance.UpLoadFileAsync("封装后上传.png", Application.streamingAssetsPath + "/test.png", (code) =>
         {
             if (code == HttpStatusCode.OK)
-                print("下载成功");
+                print("上传指令成功");
             else
-                print("下载失败" + code);
+                print("上传指令失败" + code);
         });
 
     }
--- Assets/Scripts/Manager/HttpMgr.cs
+++ Assets/Scripts/Manager/HttpMgr.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
@@ -10,6 +11,9 @@ public class HttpMgr
     public static HttpMgr Instance => instance;
 
     private string HTTP_PATH = "http://192.168.10.4:8080/HTTP_Server/";
+
+    private string USER_NAME = "cube";
+    private string PASS_WORD = "cube";
 
     /// <summary>
     /// 下载指定文件到本地指定路径中
@@ -86,5 +90,75 @@ public class HttpMgr
         });
 
         action?.Invoke(result);
+    }
+
+    /// <summary>
+    /// 上传文件到HTTP服务器
+    /// </summary>
+    /// <param name="fileName">传到远端服务器上的文件名</param>
+    /// <param name="loacalFilePath">本地的文件路径</param>
+    /// <param name="action">上传结束后的回调函数</param>
+    public async void UpLoadFileAsync(string fileName, string loacalFilePath, UnityAction<HttpStatusCode> action)
+    {
+        HttpStatusCode result = HttpStatusCode.BadRequest;
+
+        await Task.Run(() =>
+        {
+            try
+            {
+                HttpWebRequest req = HttpWebRequest.Create(HTTP_PATH) as HttpWebRequest;
+                req.Method = WebRequestMethods.Http.Post;
+                req.ContentType = "multipart/form-data;boundary=MrTang";
+                req.Timeout = 500000;
+                req.Credentials = new NetworkCredential(USER_NAME, PASS_WORD);
+                req.PreAuthenticate = true;
+
+                //拼接字符串 头部
+                string head = "--MrTang\r\n" +
+                "Content-Disposition:form-data;name=\"file\";filename=\"{0}\"\r\n" +
+                "Content-Type:application/octet-stream\r\n\r\n";
+                //替换文件名
+                head = string.Format(head, fileName);//替换 head字符串的{0} 
+
+                byte[] headBytes = Encoding.UTF8.GetBytes(head);
+
+                //尾部的边界字符串
+                byte[] endBytes = Encoding.UTF8.GetBytes("\r\n--MrTang--\r\n");
+
+                using (FileStream localStream = File.OpenRead(loacalFilePath))
+                {
+                    //设置长度
+                    req.ContentLength = headBytes.Length + localStream.Length + endBytes.Length;
+                    //写入流
+                    Stream upLoadStream = req.GetRequestStream();
+                    //写入头部
+                    upLoadStream.Write(headBytes, 0, headBytes.Length);
+                    //写入上传文件
+                    byte[] bytes = new byte[4096];
+                    int contentLenght = localStream.Read(bytes, 0, bytes.Length);
+                    while (contentLenght != 0)
+                    {
+                        upLoadStream.Write(bytes, 0, contentLenght);
+                        contentLenght = localStream.Read(bytes, 0, bytes.Length);
+                    }
+                    //写入尾部
+                    upLoadStream.Write(endBytes, 0, endBytes.Length);
+
+                    upLoadStream.Close();
+                    loacalFilePath.Clone();
+                }
+
+                HttpWebResponse res = req.GetResponse() as HttpWebResponse;
+                //让外部去处理结果 
+                result = res.StatusCode;
+                res.Close();
+            }
+            catch (WebException w)
+            {
+                Debug.Log("上传出错" + w.Status + w.Message);
+            }
+        });
+
+        action?.Invoke(result);//在Task执行结束后调用回调委托，避免在多线程中访问Unity对象
     }
 }
